202949f002c2eb432197fff3b0606f7059065489
DanielRGilles <danryangilles@gmail.com>
2021-08-13 17:15:31 -0700

    function built

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
+
+
 // import functions and grab DOM elements
+const inputEl1 = document.getElementById('input1');
+const inputEl2 = document.getElementById('input2');
+const inputEl3 = document.getElementById('input3');
+const inputEl4 = document.getElementById('input4');
+const inputEl5 = document.getElementById('input5');
+const inputEl6 = document.getElementById('input6');
+const inputEl7 = document.getElementById('input7');
+const inputEl8 = document.getElementById('input8');
+const inputEl9 = document.getElementById('input9');
+const inputEl10 = document.getElementById('input10');
+const inputEl11 = document.getElementById('input11');
+const inputEl12 = document.getElementById('input12');
+const subEl = document.getElementById('btn-sub');
+
+
+
+
+const spanEl1 = document.getElementById('year');
+const spanEl2 = document.getElementById('vehicle');
+const spanEl3 = document.getElementById('firstNoun');
+const spanEl4 = document.getElementById('price');
+const spanEl5 = document.getElementById('object');
+const spanEl6 = document.getElementById('adjective1');
+const spanEl7 = document.getElementById('number1');
+const spanEl8 = document.getElementById('number2');
+const spanEl9 = document.getElementById('number3');
+const spanEl10 = document.getElementById('noun2');
+const spanEl11 = document.getElementById('noun3');
+const spanEl12 = document.getElementById('animal');
 
 // initialize state
 
 // set event listeners 
+subEl.addEventListener('click', () => {
+    spanEl1.textContent = inputEl1.value;
+console.log()
+})
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
